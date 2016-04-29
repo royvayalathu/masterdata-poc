3f2917105f71c3b6cdc352d127e3bdf8b429a96c
royvayalathu <roy.varghese@pactera.com>
2016-04-29 14:04:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 */
+
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 // routes ======================================================================
 require('./app/routes.js')(app);
